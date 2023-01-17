f9216df773183670446b9037bff6ac895fe64123
archdla <d.a.n.202021@gmail.com>
2023-01-17 03:43:13 +0300

    just printing all forms

--- lispEvaluator.py
+++ lispEvaluator.py
@@ -2,42 +2,47 @@ from lispClasses import *
 from lispReader import funcs, symbols
 from exceptions import *
 
-def setq(form):
-    machineOp = []
 
-    print("\nSETQ START")
-    if len(form.args) != 2:
-        raise InvalidFunctionSignatureException(f'wrong arguments number of function {form.content}')
-    if form.args[0].type != AtomType.SYMB:
-        raise InvalidFunctionSignatureException(
-            f'first argument of function "{form.content}" was another type than a symbol')
-    val = evaluate(form.args[1])
-    t = val.type
-    if t == AtomType.NUM or t == AtomType.STR or t == AtomType.CONST:
-        symbols[form.args[0].content] = val.content
-        print("{")
-        for k in symbols.keys():
-            print(f'{k}: {symbols.get(k)}')
-        print("}")
-        print()
-    else:
-
-
-
-def execFunc(form: LispList):
-    print(f'|{form.type}| {form.content} ', end="")
-    if form.content in funcs:
-        match form.content:
-            case 'setq':
-
-
-
+# def setq(form):
+#     machineOp = []
+#
+#     print("\nSETQ START")
+#     if len(form.args) != 2:
+#         raise InvalidFunctionSignatureException(f'wrong arguments number of function {form.content}')
+#     if form.args[0].type != AtomType.SYMB:
+#         raise InvalidFunctionSignatureException(
+#             f'first argument of function "{form.content}" was another type than a symbol')
+#     val = evaluate(form.args[1])
+#     t = val.type
+#     if t == AtomType.NUM or t == AtomType.STR or t == AtomType.CONST:
+#         symbols[form.args[0].content] = val.content
+#         print("{")
+#         for k in symbols.keys():
+#             print(f'{k}: {symbols.get(k)}')
+#         print("}")
+#         print()
+#     else:
+#
+#
+#
+# def execFunc(form: LispList):
+#     print(f'|{form.type}| {form.content} ', end="")
+#     if form.content in funcs:
+#         match form.content:
+#             case 'setq':
+#                 setq(form)
+#
 
 
 # [evaluator]
 def evaluate(form: LispObject):
     if type(form) == LispAtom:
+        print(form, end=" ")
         return form
     else:
-        execFunc(form)
+        print(form.type, form.content)
+        for arg in form.args:
+            evaluate(arg)
+        print()
+
     return
