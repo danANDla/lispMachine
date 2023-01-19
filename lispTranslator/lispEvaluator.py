c587613a0231a3d63fc4b1657f8ce0496dd2773b
archdla <d.a.n.202021@gmail.com>
2023-01-20 02:05:43 +0300

    print function

--- lispTranslator/lispEvaluator.py
+++ lispTranslator/lispEvaluator.py
@@ -55,7 +55,7 @@ def storeValue(value: LispAtom) -> list:
 
 
 def storePrev(prev):
-    return storeValue(LispAtom('prev' + str(prev), AtomType.PREV))
+    return storeValue(LispAtom(prev, AtomType.PREV))
 
 
 def lispSetq(form):
@@ -86,6 +86,8 @@ def lispSetq(form):
 
 
 def lispSum(form: LispList):
+    if len(form.args) != 2:
+        raise InvalidFunctionSignatureException(f'wrong arguments number of function {form.content}')
     for i, a in enumerate(form.args):
         if a.type == AtomType.SYMB and symbols[a.content][0] == AtomType.UNDEF:
             raise SymbNotFoundException(f"Unknown symbol '{a.content}'")
@@ -111,6 +113,30 @@ def lispSum(form: LispList):
         machineCodes.append(createInstr(Opcode.ADD, symbols[form.args[1].content][2], 1))
     else:
         machineCodes.append(createInstr(Opcode.ADD, form.args[1].content, 0))
+    machineCodes += storeValue(form.args[0])
+    return machineCodes
+
+
+def lispPrint(form: LispList):
+    if len(form.args) != 1:
+        raise InvalidFunctionSignatureException(f'wrong arguments number of function {form.content}')
+    for i, a in enumerate(form.args):
+        if a.type == AtomType.SYMB and symbols[a.content][0] == AtomType.UNDEF:
+            raise SymbNotFoundException(f"Unknown symbol '{a.content}'")
+        if type(a) != LispAtom:
+            raise InvalidFunctionSignatureException(f'{form.content} function only works with atoms')
+        if not (a.type == AtomType.SYMB or a.type == AtomType.NUM or a.type == AtomType.PREV):
+            raise InvalidFunctionSignatureException(f'{form.content} function only works with numbers')
+        if a.type == AtomType.SYMB and not (
+                symbols[a.content] == 'NIL' or
+                symbols[a.content] == constNIL or
+                symbols[a.content][0] == AtomType.CONST or
+                symbols[a.content][0] == AtomType.NUM):
+            raise InvalidFunctionSignatureException(f'{form.content} function only works with numbers')
+
+    machineCodes = []
+    machineCodes += loadValue(form.args[0])
+    machineCodes.append(createInstr(Opcode.PRINT, '', 0))
     return machineCodes
 
 
@@ -132,6 +158,8 @@ def execFunc(form: LispList):
                 prevId += 1
             case 'setq':
                 machineCodes = lispSetq(form)
+            case 'print':
+                machineCodes = lispPrint(form)
 
     return machineCodes
 
