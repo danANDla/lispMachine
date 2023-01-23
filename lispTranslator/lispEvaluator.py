313301cf31d2516be9297adfd95b92b333330cc7
archdla <d.a.n.202021@gmail.com>
2023-01-23 04:05:47 +0300

    pep8 fix line break after binary operator

--- lispTranslator/lispEvaluator.py
+++ lispTranslator/lispEvaluator.py
@@ -123,10 +123,10 @@ def arithCheck(form: LispList) -> bool:
         if not (a.type == AtomType.SYMB or a.type == AtomType.NUM or a.type == AtomType.PREV):
             raise InvalidFunctionSignatureException(f'sum function only works with numbers')
         if a.type == AtomType.SYMB and not (
-                symbols[a.content] == 'NIL' or
-                symbols[a.content] == constNIL or
-                symbols[a.content][0] == AtomType.CONST or
-                symbols[a.content][0] == AtomType.NUM):
+                symbols[a.content] == 'NIL'
+                or symbols[a.content] == constNIL
+                or symbols[a.content][0] == AtomType.CONST
+                or symbols[a.content][0] == AtomType.NUM):
             raise InvalidFunctionSignatureException(f'sum function only works with numbers')
     return True
 
@@ -168,11 +168,11 @@ def lispPrint(form: LispList):
         if not (a.type == AtomType.SYMB or a.type == AtomType.NUM or a.type == AtomType.PREV):
             raise InvalidFunctionSignatureException(f'{form.content} function only works with numbers')
         if a.type == AtomType.SYMB and not (
-                symbols[a.content][0] == AtomType.STR or
-                symbols[a.content] == 'NIL' or
-                symbols[a.content] == constNIL or
-                symbols[a.content][0] == AtomType.CONST or
-                symbols[a.content][0] == AtomType.NUM):
+                symbols[a.content][0] == AtomType.STR
+                or symbols[a.content] == 'NIL'
+                or symbols[a.content] == constNIL
+                or symbols[a.content][0] == AtomType.CONST
+                or symbols[a.content][0] == AtomType.NUM):
             raise InvalidFunctionSignatureException(f'{form.content} function only works with numbers')
 
     machineCodes = []
